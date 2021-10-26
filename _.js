db7bc8394844acf05a8077b079ccc0e9f5449240
Robert Shields <you@example.com>
2021-10-26 01:52:01 -0700

    working on inRange lodash project

--- _.js
+++ _.js
@@ -3,8 +3,29 @@ const _ = {
       let lowerClampedValue =  Math.max(number, lower)
       let clampedValue =  Math.min(lowerClampedValue, upper);
       return clampedValue
+    },
+     inRange(number,start,end){
+        if (end === undefined){
+          end = 0
+        }
+        if (start > end) {
+          let newEnd = start
+          let newStart = end
+  
+          if (number > newStart && number < newEnd){
+            return true
+          } else {
+            return false
+          };
+        };
+        if (number > start && number < end) {
+          return true
+        } else {
+          return false
+        }
+  
+      },
     }
-  }
 
 // Do not write or modify code below this line.
 module.exports = _;
