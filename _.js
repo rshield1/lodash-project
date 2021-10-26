912f41b5dddb7d8bae5609371395a2950b3579bb
Robert Shields <you@example.com>
2021-10-26 01:27:48 -0700

    added clamp method

--- _.js
+++ _.js
@@ -1,7 +1,10 @@
-
-
-
-
+const _ = {
+     clamp(number,lower,upper){
+      let lowerClampedValue =  Math.max(number, lower)
+      let clampedValue =  Math.min(lowerClampedValue, upper);
+      return clampedValue
+    }
+  }
 
 // Do not write or modify code below this line.
 module.exports = _;
